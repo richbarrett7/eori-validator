08922978569e3bc9ac0a05a6d84393f3ff5bbebe
Chris White <81559587+chriswhite-parcelmonkey@users.noreply.github.com>
2021-07-22 14:26:20 +0100

    Update eori.php
    It changed every line coz of linting - but just really need to look at the curlopt_ssl and the additional response of 400

--- src/eori.php
+++ src/eori.php
@@ -2,83 +2,81 @@
 
 namespace eorivalidator;
 
-class eori {
-  
-  const eu_wsdl = 'https://ec.europa.eu/taxation_customs/dds2/eos/validation/services/validation?wsdl';
-  const gov_uk_url = 'https://www.tax.service.gov.uk/check-eori-number/result';
-  
-  static function validate(string $eori) {
-    
-    $eori = strtoupper($eori);
-    $sanitised = preg_replace("/[^A-Z0-9]/",'', $eori);
-    
-    if($eori != $sanitised) {
-      throw new malformedEoriNumber('The EORI should contain letters and numbers only, no spaces');
+class eori
+{
+    const eu_wsdl = 'https://ec.europa.eu/taxation_customs/dds2/eos/validation/services/validation?wsdl';
+    const gov_uk_url = 'https://www.tax.service.gov.uk/check-eori-number/result';
+
+    static function validate(string $eori)
+    {
+        $eori = strtoupper($eori);
+        $sanitised = preg_replace("/[^A-Z0-9]/", '', $eori);
+
+        if ($eori != $sanitised) {
+            throw new malformedEoriNumber('The EORI should contain letters and numbers only, no spaces');
+        }
+
+        if (!is_numeric(substr($sanitised, 2))) {
+            throw new malformedEoriNumber('The EORI should start with two letters followed by numbers');
+        }
+
+        if (!ctype_alpha(substr($sanitised, 0, 2))) {
+            throw new malformedEoriNumber('The EORI should start with two letters');
+        }
+
+        if (substr($sanitised, 0, 2) == 'GB') {
+            return self::GB($sanitised);
+        } else {
+            return self::EU($sanitised);
+        }
     }
-    
-    if(!is_numeric(substr($sanitised,2))) {
-      throw new malformedEoriNumber('The EORI should start with two letters followed by numbers');
+
+    static function EU(string $eori)
+    {
+        $params = array(
+            "eori" => $eori
+        );
+
+        try {
+            $client = new \SoapClient(self::eu_wsdl);
+            $response = $client->__soapCall("validateEORI", array($params));
+        } catch (\SoapFault $e) {
+            throw new serviceNotAvailableException('The EU web service was unavailable: ' . $e->getMessage());
+        }
+
+        return $response->return->result->statusDescr == 'Valid' ? true : false;
+
     }
-    
-    if(!ctype_alpha(substr($sanitised,0,2))) {
-      throw new malformedEoriNumber('The EORI should start with two letters');
+
+    static function GB(string $eori)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, self::gov_uk_url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('eori' => $eori)));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        $response_body = curl_exec($ch);
+        $response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($response_code !== 200) {
+            if ($response_code === 400) {
+                throw new malformedEoriNumber('Please check your EORI number');
+            } else {
+                throw new serviceNotAvailableException('The GB web service was unavailable: Response code ' . $response_code);
+            }
+        }
+
+        if (stripos($response_body, 'Invalid EORI number')) return false;
+
+        return true;
     }
-    
-    if(substr($sanitised,0,2) == 'GB') {
-      return self::GB($sanitised);
-    } else {
-      return self::EU($sanitised);
-    }
-    
-  }
-  
-  static function EU(string $eori) {
-    
-    $params = array(
-      "eori" => $eori
-    );
-    
-    try {
-      
-      $client = new \SoapClient(self::eu_wsdl);
-      $response = $client->__soapCall("validateEORI", array($params));
-      
-    } catch (\SoapFault $e) {
-      
-      throw new serviceNotAvailableException('The EU web service was unavailable: '.$e->getMessage());
-      
-    }
-    
-    return $response->return->result->statusDescr == 'Valid' ? true : false;
-        
-  }
-  
-  static function GB(string $eori) {
-        
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, self::gov_uk_url);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('eori' => $eori)));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_TIMEOUT,5);
-    
-    $response_body = curl_exec($ch);
-    $response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close ($ch);
-        
-    if($response_code != 200) {
-      throw new serviceNotAvailableException('The GB web service was unavailable: Response code '.$response_code);
-    }
-    
-    if(stripos($response_body, 'Invalid EORI number')) return false;
-    
-    return true;
-    
-  }
-    
 }
 
-class serviceNotAvailableException extends \Exception {}
-class malformedEoriNumber extends \Exception {}
-  
-?>
+class serviceNotAvailableException extends \Exception { }
+
+class malformedEoriNumber extends \Exception { }
